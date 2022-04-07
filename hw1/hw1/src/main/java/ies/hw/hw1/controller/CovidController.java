b438439cbde8a509062c0c6a384d9e304d77a893
itstar <luciusviniciusf@gmail.com>
2022-04-07 21:01:24 +0100

    CovidController getAll and byname

--- hw1/hw1/src/main/java/ies/hw/hw1/controller/CovidController.java
+++ hw1/hw1/src/main/java/ies/hw/hw1/controller/CovidController.java
@@ -1,20 +1,33 @@
 package ies.hw.hw1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ies.hw.hw1.models.Country;
 import ies.hw.hw1.service.CovidService;
 
 @RestController
 public class CovidController {
     
-
     @Autowired
     private CovidService service;
 
-    @GetMapping("/stats")
-    public String getStats() {
-        return "sus";
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        System.out.println("Getting all countries");
+        return service.getAllCountries();
     }
+
+    @GetMapping("/countries/{name}")
+    public Country getCountryByName(@PathVariable(value = "name") String name) {
+        System.out.println("Getting Country by name: " + name);
+        return service.getCountryByName(name);
+    }
+
+
 }
--- hw1/hw1/src/main/java/ies/hw/hw1/service/CovidService.java
+++ hw1/hw1/src/main/java/ies/hw/hw1/service/CovidService.java
@@ -1,8 +1,19 @@
 package ies.hw.hw1.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import ies.hw.hw1.models.Country;
 
 @Service
 public class CovidService {
     
+    public List<Country> getAllCountries() {
+        return null;
+    }
+
+    public Country getCountryByName(String name) {
+        return null;
+    }
 }
